898a3153e9aa8bb5d9a7a0379365538421dbab11
jasminew13 <jasminewu101@gmail.com>
2024-03-04 09:51:39 -0500

    imgs

--- array.js
+++ array.js
@@ -2,67 +2,67 @@ const products = [
     {
         item: "iPhone 15",
         price: 800,
-        img: "https://i5.walmartimages.com/seo/Verizon-Apple-iPhone-15-Pro-128GB-Natural-Titanium_79baf43f-cbc5-44b3-b066-5b7c88b6dfec.77e2bd5aadd90f19f91b7846d14c2b0e.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF"
+        img: "../img/iphone15.webp"
     },
     {
         item: "Luxury plane",
         price: 10000000,
-        img: "https://api.siennacharles.com/images/2021/05/11/private-plane-609a5ac871aba582929060.jpg",
+        img: "../img/luxuryplane.jpg",
     },
     {
         item: "Medium sized leather Prada bag",
         price: 2000,
-        img: "https://www.prada.com/content/dam/pradabkg_products/1/1BA/1BA434/2CY9F0002/1BA434_2CY9_F0002_V_OBO_SLR.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg",
+        img: "../img/pradabag.avif",
     },
     {
         item: "La Mer Hydrating Infused Emulsion",
         price: 300,
-        img: "https://www.cremedelamer.com/media/export/cms/products/responsive/lm_sku_5XPX01_4x5_0.png?width=900&height=1125",
+        img: "../img/lamer.avif",
     },
     {
         item: "Lamborgini",
         price: 500000,
-        img: "https://cdn.motor1.com/images/mgl/6ZAvXk/s1/lamborghini-invencible.webp",
+        img: "../img/lambo.webp",
     },
     {
         item: "Bora Barrier™ Repair Cream",
         price: 70,
-        img: "https://www.gopicky.com/_next/image?url=https%3A%2F%2Fmedia.gopicky.com%2Fproducts%2Fimages%2FDrunkElephant-BoraBarrierRepairCream-473047342&w=3840&q=75",
+        img: "../img/repaircream.webp",
     },
     {
         item: "Pokemon Fusion Strike Booster Pack",
         price: 5,
-        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQSLOuEAyVICCltLc1nS5SuqWA_68vjmNHpWhD0zeW0ZAb_Me3ATCnjjzAFsvOXRkRv5Xw81tXS9WFi4oHlnMID3eJ7MME4",
+        img: "../img/pokemoncard",
     },
     {
         item: "Franchini FIE E. Foil Blade (Bare)",
         price: 250,
-        img: "https://www.absolutefencinggear.com/media/catalog/product/cache/43d5ff4e03a4ae9f104dcc33033db194/i/m/img_7077.jpg",
+        img: "../img/fieblade.jpg",
     },
     {
         item: "Taylor Swift's Signed Guitar",
         price: 1200,
-        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSTvxRoKuhfErpdNhTtpkwS-l1yRuIUWRXawQrIbonUSmqbdYNGWebm0n4n7Pe05xDa-lTIJSXqfxtLycwlBxSFtZs5mQj_rnzbUb72kSqEuhUcnvOyC2ePxw",
+        img: "../img/signedguitar.webp",
     },
     {
         item: "Grand Curved Sectional Couch",
         price: 5000,
-        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQPfC9bGvlx2SfztrhI_qLV0q6laG8GvbnWlAA0T9TH1RzLmOFmr9rNZXJ9Z-iyCei_B16Wr6bSQfrUjIGCMHYE3dhPQbq5URweeu3qGrk",
+        img: "../img/couch",
     },
     {
         item: "Average rent in NYC for a studio apartment, per month",
         price: 3500,
-        img: "https://www.theagencyre.com/blog/wp-content/uploads/2014/09/unit_2142387_studio-apartment.jpeg",
+        img: "../img/nycapt.jpeg",
     },
     {
         item: "Alienware Aurora R16 Desktop 13th Gen Intel Core i7",
         price: 1600,
-        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcThVduatnn0GcshKzuZgfk2GER0MeKUFD8pEotwjCZv33yPa_EPWIgg_hyNriefW4vsRXyWWTJR--z4UyTLBkyRoS2uOswN",
+        img: "../img/comp.webp",
     },
     {
         item: "Seahawk Yacht",
         price: 10000000,
-        img: "https://cdn.boatinternational.com/convert/listing/2023/07/65fe7db0-2d1e-11ee-9b1e-dbef54aeb7b2-SEAHAWK_superyacht_for_sale_charter_01.webp/r%5Bheight%5D=350/65fe7db0-2d1e-11ee-9b1e-dbef54aeb7b2-SEAHAWK_superyacht_for_sale_charter_01.webp",
+        img: "../img/yacht.webp",
     },
 ]
 export {products}
